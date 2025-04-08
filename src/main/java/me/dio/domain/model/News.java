90b7b6b18c1823a4885464a48f2c359faedd7871
Deivid <deivid_marcio3110@hotmail.com>
2025-04-07 22:02:55 -0300

    alterando Classe News

--- src/main/java/me/dio/domain/model/News.java
+++ src/main/java/me/dio/domain/model/News.java
@@ -1,33 +1,8 @@
 package me.dio.domain.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
-@Entity
-public class News {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    
-    private String icon;
-    private String description;
-    
-    // Constructors, getters and setters
-    public News() {}
-    
-    public News(Long id, String icon, String description) {
-        this.id = id;
-        this.icon = icon;
-        this.description = description;
-    }
-    
-    // Getters and setters
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-    public String getIcon() { return icon; }
-    public void setIcon(String icon) { this.icon = icon; }
-    public String getDescription() { return description; }
-    public void setDescription(String description) { this.description = description; }
+@Entity(name = "tb_news")
+public class News extends BaseItem {
+
 }
